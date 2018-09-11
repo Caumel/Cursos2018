938d784145a9a5473f0729f6046afd0d556861ff
Luis Caumel <luiscaumel@gmail.com>
2018-09-11 09:32:09 +0200

    Created the project with maven spring and MyBatis

---
+++ src/main/java/com/example/cursos2018/Cursos2018Application.java
@@ -0,0 +1,12 @@
+package com.example.cursos2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursos2018Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursos2018Application.class, args);
+    }
+}
